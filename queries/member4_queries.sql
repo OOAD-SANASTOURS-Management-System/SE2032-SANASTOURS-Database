7e403ac3e05157d600146396110e2f176720e665
IT25104057 <IT25104057@my.sliit.lk>
2026-09-18 23:51:19 +0530

    Add Driver and Vehicle database implementation

--- queries/member4_queries.sql
+++ queries/member4_queries.sql
@@ -1,1 +1,7 @@
 USE sanastours_db;
+
+SELECT * FROM Driver;
+SELECT * FROM Vehicle;
+SELECT * FROM Trip;
+SELECT * FROM Booking;
+SELECT * FROM Payment;
--- schema/payment_trip.sql
+++ schema/payment_trip.sql
@@ -1,2 +1,63 @@
 CREATE DATABASE sanastours_db;
 USE sanastours_db;
+
+CREATE TABLE Driver (
+driver_id INT PRIMARY KEY,
+driver_name VARCHAR(100) NOT NULL,
+license_number VARCHAR(50)  NOT NULL UNIQUE,
+driver_status VARCHAR(50)  NOT NULL DEFAULT 'Active'
+);
+ 
+CREATE TABLE Vehicle (
+vehicle_id INT PRIMARY KEY,
+registration_no VARCHAR(50) NOT NULL UNIQUE,
+vehicle_type VARCHAR(50) NOT NULL,
+capacity INT NOT NULL,
+vehicle_status VARCHAR(50) NOT NULL DEFAULT 'Available'
+);
+
+CREATE TABLE Booking (
+booking_id INT PRIMARY KEY,
+booking_date DATE,
+travel_date DATE,
+total_amount DECIMAL(10, 2),
+booking_status VARCHAR(50),
+number_of_people INT
+);
+
+CREATE TABLE Trip (
+trip_id	INT PRIMARY KEY,
+booking_id INT NOT NULL,
+vehicle_id INT NOT NULL,
+driver_id INT NOT NULL,
+departure_datetime DATETIME NOT NULL,
+return_datetime DATETIME,
+pickup_location VARCHAR(150),
+dropoff_location VARCHAR(150),
+trip_status VARCHAR(20) NOT NULL DEFAULT 'Scheduled',
+
+CONSTRAINT fk_trip_booking
+FOREIGN KEY (booking_id) REFERENCES Booking(booking_id)
+ON DELETE CASCADE ON UPDATE CASCADE,
+ 
+CONSTRAINT fk_trip_vehicle
+FOREIGN KEY (vehicle_id) REFERENCES Vehicle(vehicle_id)
+ON DELETE RESTRICT ON UPDATE CASCADE,
+ 
+CONSTRAINT fk_trip_driver
+FOREIGN KEY (driver_id) REFERENCES Driver(driver_id)
+ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE Payment (
+payment_id INT PRIMARY KEY,
+booking_id INT NOT NULL,
+amount DECIMAL (10, 2),
+payment_date DATE,
+payment_method VARCHAR(50),
+payment_status VARCHAR(50),
+
+CONSTRAINT fk_payment_booking
+FOREIGN KEY (booking_id) REFERENCES Booking(booking_id)
+ON DELETE CASCADE ON UPDATE CASCADE
+);
